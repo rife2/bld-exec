df9aee7ca3d4315e484e30b61267514fec3518b2
Erik C. Thauvin <erik@thauvin.net>
2024-07-22 17:01:34 -0700

    Bumped bld to version 2.0.0-SNAPSHOT

--- src/bld/java/rife/bld/extension/ExecOperationBuild.java
+++ src/bld/java/rife/bld/extension/ExecOperationBuild.java
@@ -25,8 +25,7 @@ import rife.bld.publish.PublishScm;
 import java.io.IOException;
 import java.util.List;
 
-import static rife.bld.dependencies.Repository.MAVEN_CENTRAL;
-import static rife.bld.dependencies.Repository.RIFE2_RELEASES;
+import static rife.bld.dependencies.Repository.*;
 import static rife.bld.dependencies.Scope.compile;
 import static rife.bld.dependencies.Scope.test;
 import static rife.bld.operations.JavadocOptions.DocLinkOption.NO_MISSING;
@@ -35,20 +34,20 @@ public class ExecOperationBuild extends Project {
     public ExecOperationBuild() {
         pkg = "rife.bld.extension";
         name = "ExecOperation";
-        version = version(1, 0, 1);
+        version = version(1, 0, 2, "SNAPSHOT");
 
         javaRelease = 17;
 
         downloadSources = true;
         autoDownloadPurge = true;
 
-        repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
+        repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
         scope(compile)
-                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 1)));
+                .include(dependency("com.uwyn.rife2", "bld", version(2, 0, 0, "SNAPSHOT")));
         scope(test)
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 2)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 2)))
-                .include(dependency("org.assertj", "assertj-core", version(3, 26, 0)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 3)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 3)))
+                .include(dependency("org.assertj", "assertj-core", version(3, 26, 3)));
 
         javadocOperation()
                 .javadocOptions()
@@ -91,14 +90,14 @@ public class ExecOperationBuild extends Project {
     }
 
     @BuildCommand(summary = "Generates JaCoCo Reports")
-    public void jacoco() throws IOException {
+    public void jacoco() throws Exception {
         new JacocoReportOperation()
                 .fromProject(this)
                 .execute();
     }
 
     @BuildCommand(summary = "Runs PMD analysis")
-    public void pmd() {
+    public void pmd() throws Exception {
         new PmdOperation()
                 .fromProject(this)
                 .failOnViolation(true)
