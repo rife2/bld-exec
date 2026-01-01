64b4683fe2f161f91d616f9549bc1c0cfda26998
Erik C. Thauvin <erik@thauvin.net>
2026-01-01 15:04:22 -0800

    Update copyright years

--- src/bld/java/rife/bld/extension/ExecOperationBuild.java
+++ src/bld/java/rife/bld/extension/ExecOperationBuild.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 the original author or authors.
+ * Copyright 2023-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -91,15 +91,15 @@ public class ExecOperationBuild extends Project {
                 .signPassphrase(property("sign.passphrase"));
     }
 
+    public static void main(String[] args) {
+        new ExecOperationBuild().start(args);
+    }
+
     @Override
     public void test() throws Exception {
         var op = testOperation().fromProject(this);
         op.testToolOptions().reportsDir(new File("build/test-results/test/"));
         op.execute();
-    }
-
-    public static void main(String[] args) {
-        new ExecOperationBuild().start(args);
     }
 
     @BuildCommand(summary = "Runs PMD analysis")
--- src/main/java/rife/bld/extension/ExecOperation.java
+++ src/main/java/rife/bld/extension/ExecOperation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2025 the original author or authors.
+ * Copyright 2023-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,7 +92,7 @@ public class ExecOperation extends AbstractOperation<ExecOperation> {
     }
 
     static boolean isWindows(String osName) {
-        String normalized = normalizeOSName(osName);
+        var normalized = normalizeOSName(osName);
         return normalized.contains("windows") || normalized.startsWith("win");
     }
 
