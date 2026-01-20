a52b5deef0da47d4e2004b8a331f057e28b1bc20
Erik C. Thauvin <erik@thauvin.net>
2026-01-20 13:36:08 -0800

    Use IOUtils to resolve test results directory

--- src/bld/java/rife/bld/extension/ExecOperationBuild.java
+++ src/bld/java/rife/bld/extension/ExecOperationBuild.java
@@ -18,11 +18,11 @@ package rife.bld.extension;
 
 import rife.bld.BuildCommand;
 import rife.bld.Project;
+import rife.bld.extension.tools.IOUtils;
 import rife.bld.publish.PublishDeveloper;
 import rife.bld.publish.PublishLicense;
 import rife.bld.publish.PublishScm;
 
-import java.io.File;
 import java.util.List;
 
 import static rife.bld.dependencies.Repository.*;
@@ -30,6 +30,7 @@ import static rife.bld.dependencies.Scope.*;
 import static rife.bld.operations.JavadocOptions.DocLinkOption.NO_MISSING;
 
 public class ExecOperationBuild extends Project {
+
     public ExecOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-exec";
@@ -96,7 +97,7 @@ public class ExecOperationBuild extends Project {
     @Override
     public void test() throws Exception {
         var op = testOperation().fromProject(this);
-        op.testToolOptions().reportsDir(new File("build/test-results/test/"));
+        op.testToolOptions().reportsDir(IOUtils.resolveFile(buildDirectory(), "test-results", "test"));
         op.execute();
     }
 
