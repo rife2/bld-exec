5f1b8703524a91a790acacea56fec74e7762d19e
Erik C. Thauvin <erik@thauvin.net>
2026-05-23 20:48:48 -0700

    1.4.0-SNAPSHOT

--- src/bld/java/rife/bld/extension/ExecOperationBuild.java
+++ src/bld/java/rife/bld/extension/ExecOperationBuild.java
@@ -34,7 +34,7 @@ public class ExecOperationBuild extends Project {
     public ExecOperationBuild() {
         pkg = "rife.bld.extension";
         name = "bld-exec";
-        version = version(1, 3, 5, "SNAPSHOT");
+        version = version(1, 4, 0, "SNAPSHOT");
 
         javaRelease = 17;
 
