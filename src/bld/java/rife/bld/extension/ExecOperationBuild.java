8409c2318ab1240691c89a227ecc3b1c11c08410
Erik C. Thauvin <erik@thauvin.net>
2026-09-09 20:16:30 -0700

    Bump Extensions Tools to version 1.3.0

--- src/bld/java/rife/bld/extension/ExecOperationBuild.java
+++ src/bld/java/rife/bld/extension/ExecOperationBuild.java
@@ -46,7 +46,7 @@ public class ExecOperationBuild extends Project {
         var junit = version(6, 1, 3);
         scope(compile)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-tools",
-                        version(1, 3, 0, "SNAPSHOT")))
+                        version(1, 3, 0)))
                 .include(dependency("com.uwyn.rife2", "bld",
                         version(2, 3, 1, "SNAPSHOT")));
         scope(provided)
